6658b0580d44014121c6fe0b72b0353bd2707a10
ThePope05 <swobben05@gmail.com>
2023-01-10 11:19:40 +0100

    added return function to read file, and added index

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Table</title>
+</head>
+<body>
+    <table>
+        <tr>
+            <th>Merk</th>
+            <th>Model</th>
+            <th>Topsnelheid</th>
+            <th>Prijs</th>
+            <th>Delete</th>
+        </tr>
+    </table>
+    <?php
+        include("read.php");
+
+        $allCars = getInfo();
+    ?>
+</body>
+</html>
--- read.php
+++ read.php
@@ -1,19 +1,21 @@
 <?php
 
-$dsn = "mysql:host=localhost;dbname=php-pdo-crud-toets;charset=UTF8";
+function getInfo(){
+    $dsn = "mysql:host=localhost;dbname=php-pdo-crud-toets;charset=UTF8";
 
-try {
-    $pdo = new PDO($dsn, "root", "15122005");
-} catch(PDOException $e){
-    echo $e->getMessage();
+    try {
+        $pdo = new PDO($dsn, "root", "15122005");
+    } catch(PDOException $e){
+        echo $e->getMessage();
+    }
+
+    $sql = "SELECT * FROM dureauto";
+
+    $statement = $pdo->prepare($sql);
+
+    $statement->execute();
+
+    $result = $statement->fetchAll(PDO::FETCH_OBJ);
+
+    return $result;
 }
-
-$sql = "SELECT * FROM dureauto";
-
-$statement = $pdo->prepare($sql);
-
-$statement->execute();
-
-$result = $statement->fetchAll(PDO::FETCH_OBJ);
-
-var_dump($result);
