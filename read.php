5051ecb7c701d6da9dfd7038f0c76f8bcc36a333
ThePope05 <swobben05@gmail.com>
2023-01-10 11:13:01 +0100

    Basis read file

--- read.php
+++ read.php
@@ -1,2 +1,19 @@
 <?php
 
+$dsn = "mysql:host=localhost;dbname=php-pdo-crud-toets;charset=UTF8";
+
+try {
+    $pdo = new PDO($dsn, "root", "15122005");
+} catch(PDOException $e){
+    echo $e->getMessage();
+}
+
+$sql = "SELECT * FROM dureauto";
+
+$statement = $pdo->prepare($sql);
+
+$statement->execute();
+
+$result = $statement->fetchAll(PDO::FETCH_OBJ);
+
+var_dump($result);
